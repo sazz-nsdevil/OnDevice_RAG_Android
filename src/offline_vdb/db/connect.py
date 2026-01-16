9607105b2f52dc1f8d57ed7323cfff5acf09b22d
madhav <madav@nsdevil.com>
2026-01-16 15:25:08 +0545

    fixed the error

--- src/offline_vdb/db/connect.py
+++ src/offline_vdb/db/connect.py
@@ -1,11 +1,11 @@
 import sqlite3
 def connect(db_path:str)->sqlite3.Connection:
-    con.sqlite3.connect(db_path)
+    con=sqlite3.connect(db_path)
     con.row_factory=sqlite3.Row
     con.execute("PRAGMA foreign_keys=ON;")
     con.execute("PRAGMA journal mode=wal;")
     con.execute("PRAGMA synchronus=NORMAL;")
     con.execute("PRAGME temp_score=MEMORY;")
-    con.execute("PRAGME cache_size=-20000;")
+    con.execute("PRAGME cache_size=-20000;") # which is approximate 20 mb
 
     return con
